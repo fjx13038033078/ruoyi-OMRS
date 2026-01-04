6b7b70d9a89dcf82850a83a67181d9a9686cfda7
范佳兴 <c1111111c@163.com>
2026-01-04 14:59:52 +0800

    fix:修改系统名称

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -159,7 +159,7 @@ export default {
               left: 'center',
               top: 'center',
               style: {
-                text: '大学生学籍管理平台',
+                text: '穿搭推荐管理系统',
                 fontSize: 80,
                 fontWeight: 'bold',
                 lineDash: [0, 200],
