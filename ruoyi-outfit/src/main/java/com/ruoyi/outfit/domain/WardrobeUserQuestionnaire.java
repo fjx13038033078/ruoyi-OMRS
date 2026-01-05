ba997e495d373da4edf9bd2c8ef8c567d22dec0c
范佳兴 <c1111111c@163.com>
2026-01-05 15:52:00 +0800

    fix:实体类代码规范化

--- ruoyi-outfit/src/main/java/com/ruoyi/outfit/domain/WardrobeUserQuestionnaire.java
+++ ruoyi-outfit/src/main/java/com/ruoyi/outfit/domain/WardrobeUserQuestionnaire.java
@@ -1,90 +1,56 @@
 package com.ruoyi.outfit.domain;
 
-import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
- * 用户问卷记录对象 wardrobe_user_questionnaire
+ * 用户问卷记录实体类
  *
  * @author fanjiaxing
  */
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@TableName("wardrobe_user_questionnaire")
 public class WardrobeUserQuestionnaire extends BaseEntity {
+
     private static final long serialVersionUID = 1L;
 
-    /** 主键ID */
+    /**
+     * 主键ID
+     */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
-    /** 用户ID */
+    /**
+     * 用户ID
+     */
     @Excel(name = "用户ID")
     private Long userId;
 
-    /** 问题编码 */
+    /**
+     * 问题编码
+     */
     @Excel(name = "问题编码")
     private String questionCode;
 
-    /** 问题内容 */
+    /**
+     * 问题内容
+     */
     @Excel(name = "问题内容")
     private String questionContent;
 
-    /** 回答内容 */
+    /**
+     * 回答内容
+     */
     @Excel(name = "回答内容")
     private String answerContent;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setQuestionCode(String questionCode) {
-        this.questionCode = questionCode;
-    }
-
-    public String getQuestionCode() {
-        return questionCode;
-    }
-
-    public void setQuestionContent(String questionContent) {
-        this.questionContent = questionContent;
-    }
-
-    public String getQuestionContent() {
-        return questionContent;
-    }
-
-    public void setAnswerContent(String answerContent) {
-        this.answerContent = answerContent;
-    }
-
-    public String getAnswerContent() {
-        return answerContent;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-                .append("id", getId())
-                .append("userId", getUserId())
-                .append("questionCode", getQuestionCode())
-                .append("questionContent", getQuestionContent())
-                .append("answerContent", getAnswerContent())
-                .append("createTime", getCreateTime())
-                .append("updateTime", getUpdateTime())
-                .toString();
-    }
 }
-
