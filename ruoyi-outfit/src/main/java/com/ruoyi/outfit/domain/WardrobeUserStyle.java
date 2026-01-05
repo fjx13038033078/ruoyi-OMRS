aca16f513740be93e1b7ca431f3815dbb9a95cce
范佳兴 <c1111111c@163.com>
2026-01-05 15:45:07 +0800

    fix:修改实体类代码规范

--- ruoyi-outfit/src/main/java/com/ruoyi/outfit/domain/WardrobeUserStyle.java
+++ ruoyi-outfit/src/main/java/com/ruoyi/outfit/domain/WardrobeUserStyle.java
@@ -1,89 +1,56 @@
 package com.ruoyi.outfit.domain;
 
-import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
- * 用户风格偏好对象 wardrobe_user_style
+ * 用户风格偏好实体类
  *
  * @author ruoyi
  */
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@TableName("wardrobe_user_style")
 public class WardrobeUserStyle extends BaseEntity {
+
     private static final long serialVersionUID = 1L;
 
-    /** 主键ID */
+    /**
+     * 主键ID
+     */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
-    /** 用户ID */
+    /**
+     * 用户ID
+     */
     @Excel(name = "用户ID")
     private Long userId;
 
-    /** 风格编码 */
+    /**
+     * 风格编码
+     */
     @Excel(name = "风格编码")
     private String styleCode;
 
-    /** 风格名称 */
+    /**
+     * 风格名称
+     */
     @Excel(name = "风格名称")
     private String styleName;
 
-    /** 偏好程度(1-5) */
+    /**
+     * 偏好程度(1-5)
+     */
     @Excel(name = "偏好程度")
     private Integer preferenceLevel;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setStyleCode(String styleCode) {
-        this.styleCode = styleCode;
-    }
-
-    public String getStyleCode() {
-        return styleCode;
-    }
-
-    public void setStyleName(String styleName) {
-        this.styleName = styleName;
-    }
-
-    public String getStyleName() {
-        return styleName;
-    }
-
-    public void setPreferenceLevel(Integer preferenceLevel) {
-        this.preferenceLevel = preferenceLevel;
-    }
-
-    public Integer getPreferenceLevel() {
-        return preferenceLevel;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-                .append("id", getId())
-                .append("userId", getUserId())
-                .append("styleCode", getStyleCode())
-                .append("styleName", getStyleName())
-                .append("preferenceLevel", getPreferenceLevel())
-                .append("createTime", getCreateTime())
-                .toString();
-    }
 }
-
